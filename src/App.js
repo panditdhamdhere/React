030e92efb6cb30a541f43074502328e93cf99880
pandit <dhamdherepandit@gmail.com>
2022-10-27 16:19:22 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar'
+import Textform from './components/Textform'
 function App() {
   return (
     <>
       <Navbar title="TextUtils" about="aboutText"/>
+      <div className="container">
+      <Textform heading="Enter The Text To Analyze"/>
+      </div>
     </>
   );
 }
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
-export default function Textform() {
+export default function Textform(props) {
   return (
     <div>
-      
+        <h1>{props.heading}</h1>
+<div className="mb-3">
+  <label for="myBox" className="form-label">Example textarea</label>
+  <textarea className="form-control" id="myBox" rows="6"></textarea>
+</div>
     </div>
   )
 }
